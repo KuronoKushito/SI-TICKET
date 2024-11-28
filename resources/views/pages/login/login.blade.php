27a7984b15e578478d3f31b3e9ced6d9c5755b39
FerdiNurrahman <ferdiart113@gmail.com>
2024-11-28 15:29:53 +0800

    Add dashboard Template

--- resources/views/pages/login/login.blade.php
+++ resources/views/pages/login/login.blade.php
@@ -36,7 +36,7 @@
               <h3><strong>Login</strong></h3>
               <!-- <p class="mb-4">Lorem ipsum dolor sit amet elit. Sapiente sit aut eos consectetur adipisicing.</p> -->
               </div>
-              <form action="#" method="post">
+              <form action="{{ route('dashboard') }}" method="GET">
                 <div class="form-group first">
                   <label for="username">Username</label>
                   <input type="text" class="form-control" placeholder="your-email@gmail.com" id="username">
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\landingPage\LandingPageController;
 use App\Http\Controllers\Login\LoginController;
+use App\Http\Controllers\Admin\AdminController;
 
 Route::get('/', [LandingPageController::class, 'beranda']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
+
+
+Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
