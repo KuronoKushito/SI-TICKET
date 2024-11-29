70ef3025f24f8525f924dbe86452aaadbf494d0f
Harlan <harlanmuradi@gmail.com>
2024-11-29 16:25:12 +0800

    model modification

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
 
+    protected $table = "user";
     protected $primaryKey = 'iduser';
     protected $fillable = ['iddepartment', 'username', 'password', 'level'];
     public function department()
