3afa5a111afd5082d15f6e0b6096f36bc69d1484
Harlan <harlanmuradi@gmail.com>
2024-11-29 16:10:08 +0800

    adding migration

--- database/migrations/2024_11_29_080440_create_ticket.php
+++ database/migrations/2024_11_29_080440_create_ticket.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('phonenumber');
             $table->text('trouble');
             $table->timestamps();
+            //
         });
     }
 
