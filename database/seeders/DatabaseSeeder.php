f7adb778da921d1e3b5ef977f8b1913052036f68
Harlan <harlanmuradi@gmail.com>
2024-11-29 16:19:13 +0800

    seeders making

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +17,27 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            [
+                'username' => 'admin',
+                'password' => Hash::make('Jhonlin@123'),
+                'level' => 'admin',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'username' => 'sap-department',
+                'password' => Hash::make('Jhonlin@123'),
+                'level' => 'department',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'username' => 'it-department',
+                'password' => Hash::make('Jhonlin@123'),
+                'level' => 'department',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
